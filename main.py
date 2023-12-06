d88123175e612fbe178f136ddf8c0cf88023115f
mohanchi <chimohan@outlook.com>
2023-12-06 00:57:26 -0500

    Add GetParticularRecipeCategories function

--- main.py
+++ main.py
@@ -2,25 +2,55 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from openai import OpenAI
 import openai
+import json
 
-openai.api_key = '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'
+# openai.api_key = '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'
+openai.api_key = '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'
 
 app = Flask(__name__)
 cors = CORS(app)
+
+EXAMPLE_RESULT_CATEGORY = {
+  "categories": [
+    {
+      "Ingredient": ["Ground Beef", "Italian Sausage", "Chicken", "Shrimp", "Salmon", "Pancetta", "Tomatoes",
+                     "Garlic", "Onion", "Bell Peppers", "Mushrooms", "Spinach", "Zucchini", "Cherry Tomatoes",
+                     "Parmesan", "Mozzarella", "Ricotta", "Pecorino", "Gorgonzola"],
+      "Cuisine": ["Italian", "American", "Asian", "Mediterranean", "Latin American"],
+      "PreparationTime": ["10mins", "20mins"],
+      "CookingMethod": ["Frying", "Stewing", "Boiling", "Baking"],
+      "AllergenInformation": ["Wheat", "Gluten", "Eggs", "Milk", "Soy", "Tree Nuts", "Peanuts", "Shellfish", "Fish"],
+      "DietaryConsideration": ["Whole Grain", "Vegetarian", "High-Protein", "Vegan", "Gluten-Free"],
+      "Varieties": ["Spaghetti", "Penne", "Fusilli", "Farfalle", "Rigatoni", "Fettuccine", "Linguine", "Orzo",
+                    "Ravioli", "Tortellini"]
+    }
+  ]
+}
+
+client = OpenAI(api_key=openai.api_key)
+input_value = 'pasta'
 
 @app.route('/GetRecipeCategories', methods = ['POST'])
 def GetRecipeCategories():
     input_value = request.form['input']
     print(input_value)
     # prompt = 'Give me the categories for this recipe (such as ingredient, cuisine, ...): ' + request.values.get('input')
-    prompt = 'Give me the categories for recipe (such as ingredient, cuisine, preparation time ...)：'+ input_value
+    prompt = 'Give me the categories for this dish：'+ input_value
     # client = OpenAI()
-    client = OpenAI(api_key=openai.api_key)
+    example_string= json.dumps(EXAMPLE_RESULT_CATEGORY, indent=2)
+
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
         response_format={"type": "json_object"},
         messages=[
-            {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+            {"role": "system", "content":
+                "You are a helpful assistant designed to output JSON. The JSON should be 3 layers."
+                "Provide me with the categories for recipe generation of pasta"
+                "including aspects such as ingredients, cuisine, preparation time, and more. "
+                "Specifically, within the category, list the relevant subcategories for recipe generation."
+             },
+            {"role": "user", "content": "Give me the categories for this dish：pasta"},
+            {"role": "assistant", "content": example_string},
             {"role": "user", "content": prompt}
         ]
     )
@@ -28,10 +58,28 @@ def GetRecipeCategories():
     result_json = response.choices[0].message.content
     return result_json
 
-@app.route('/GetParticularRecipeCategories', methods = ['GET'])
+@app.route('/GetParticularRecipeCategories', methods = ['POST'])
 def GetParticularRecipeCategories():
-    #test_1
-    return 0
+    user_json_data = request.get_json()
+    categories = user_json_data.get("categories", [])
+    prompt = f"Give me a Recipe of {input_value}\n"
+    for category in categories:
+        for key, values in category.items():
+            prompt += f"{key} is {', '.join(values)}\n"
+    print("prompt: " + prompt)
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        response_format={"type": "text"},
+        messages=[
+            {"role": "system", "content":
+                "You are a helpful assistant designed to output a customer recipe text."
+             },
+            {"role": "user", "content": prompt}
+        ]
+    )
+    customized_recipe = response.choices[0].message.content
+    print("customized recipe: " + customized_recipe)
+    return customized_recipe
 
 
 if __name__ == '__main__':
